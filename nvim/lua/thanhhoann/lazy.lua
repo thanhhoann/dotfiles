12c02c6b8e3e229aecd9f2c597105fbc561106b4
thanhhoann <pdth456@gmail.com>
2024-03-06 01:18:52 +0700

    testing neogit

--- nvim/lua/thanhhoann/lazy.lua
+++ nvim/lua/thanhhoann/lazy.lua
@@ -98,6 +98,18 @@ require 'lazy'.setup({
     --          ╰─────────────────────────────────────────────────────────╯
     { 'tpope/vim-fugitive' },
     { 'lewis6991/gitsigns.nvim' },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+
+            -- Only one of these is needed, not both.
+            "nvim-telescope/telescope.nvim", -- optional
+            "ibhagwan/fzf-lua",      -- optional
+        },
+        config = true
+    },
 
     --          ╭─────────────────────────────────────────────────────────╮
     --          │                       TREESITTER                        │
