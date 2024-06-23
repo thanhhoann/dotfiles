5a961289695819b0eb6493a3a5774e8c73fc3ae6
thanhhoann <pdth456@gmail.com>
2024-06-24 02:00:23 +0700

    refactor: delete redundancies

--- nvim.bak/lua/thanh_hoan/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- copy and paste to clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
-
--- just calling Format()
-vim.keymap.set("n", "<leader>f", ":Format<CR>")
